559616dee7c055580d91c246aadf376497d8f61d
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2024-11-02 16:43:41 +0100

    Create basic structure for the CLI

--- src/robocop/cli.py
+++ src/robocop/cli.py
@@ -1,2 +1,24 @@
-def main():
+import typer
+
+
+app = typer.Typer(
+    help="Static code analysis tool (linter) and code formatter for Robot Framework. "
+    "Full documentation available at https://robocop.readthedocs.io .",
+    rich_markup_mode="rich"
+)
+
+
+@app.command(name="check")
+def check_files():
+    """Lint files."""
     pass
+
+
+@app.command(name="format")
+def format_files():
+    """Format files."""
+    pass
+
+
+def main():
+    app()
