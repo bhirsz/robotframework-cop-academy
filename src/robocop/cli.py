d7b9d69b57eefeffec78d3b885b73bc4ae782846
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2024-11-03 14:17:19 +0100

    Prepare basic structure of Robocop app

--- src/robocop/cli.py
+++ src/robocop/cli.py
@@ -1,4 +1,10 @@
+from os.path import exists
+from pathlib import Path
+from typing import Annotated
+
 import typer
+
+from robocop.config import ConfigManager
 
 
 app = typer.Typer(
@@ -8,26 +14,64 @@ app = typer.Typer(
 )
 
 
+config_option = Annotated[
+    Path,
+    typer.Option(
+        help="Path to configuration file. It will overridden any configuration file found in the project.",
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+    ),
+]
+project_root_option = Annotated[
+    Path,
+    typer.Option(
+        help="Project root directory. It is used to find default configuration directory",
+        show_default="Automatically found from the sources and current working directory.",
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        readable=True,
+        resolve_path=True,
+    ),
+]
+
+
 @app.command(name="check")
-def check_files():
+def check_files(
+    config: config_option = None,
+    ignore_git_dir: Annotated[bool, typer.Option()] = False,
+    skip_gitignore: Annotated[bool, typer.Option()] = False,
+    root: project_root_option = None,
+) -> None:
     """Lint files."""
-    pass
+    config_manager = ConfigManager(
+        config=config, root=root, ignore_git_dir=ignore_git_dir, skip_gitignore=skip_gitignore
+    )
 
 
 @app.command(name="format")
-def format_files():
+def format_files(
+    config: config_option = None,
+    ignore_git_dir: Annotated[bool, typer.Option()] = False,
+    skip_gitignore: Annotated[bool, typer.Option()] = False,
+    root: project_root_option = None,
+) -> None:
     """Format files."""
-    pass
+    config_manager = ConfigManager(
+        config=config, root=root, ignore_git_dir=ignore_git_dir, skip_gitignore=skip_gitignore
+    )
 
 
 @app.command(name="list")
-def list_rules_or_formatters():
+def list_rules_or_formatters() -> None:
     """List available rules or formatters."""
-    pass
 
 
 # list configurables, reports, rules, formatters
 
 
-def main():
+def main() -> None:
     app()
