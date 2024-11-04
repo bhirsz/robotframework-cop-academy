1fe37eb32e4da65f63232b76fd93d9610e3ba246
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2024-11-04 11:27:51 +0100

    Copy Robocop & POC of first command (list rules) (#8)
    * Initalize project with uv
    
    * Create basic structure for the CLI
    
    * Create configuration classes
    
    * Prepare basic structure of Robocop app
    
    * Update project dependencies
    
    * Move Robocop linter to robocop/linter
    
    * fix: imports after moving robocop linter files
    
    * feat: add robocop list rules command

--- src/robocop/linter/runner.py
+++ src/robocop/linter/runner.py
@@ -1,1 +1,27 @@
-class Robocop: ...
+from typing import TYPE_CHECKING
+
+from robocop.linter import rules
+
+if TYPE_CHECKING:
+    from robocop.linter.rules import Rule  # TODO: Check if circular import will not happen
+
+
+class Linter:
+    def __init__(self):
+        self.checkers: list = []  # [type[BaseChecker]]
+        self.rules: dict[str, Rule] = {}
+        self.load_checkers()
+        # load_reports()
+        # configure reports / rules
+        # check for disabled rules
+
+    def load_checkers(self):
+        self.checkers = []
+        self.rules = {}
+        rules.init(self)
+
+    def register_checker(self, checker):  # [type[BaseChecker]]
+        for rule_name, rule in checker.rules.items():
+            self.rules[rule_name] = rule
+            self.rules[rule.rule_id] = rule
+        self.checkers.append(checker)
